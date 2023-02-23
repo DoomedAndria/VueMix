1d76530d1152fe9183d17e6016e098ad2c1a576e
DoomedAndria <103760839+DoomedAndria@users.noreply.github.com>
2023-02-23 17:35:38 +0400

    update

--- app2.py
+++ app2.py
@@ -1,43 +1,46 @@
 api_key = 'AIzaSyCWjReqFeZbbopqMtApZuUzpnz0_D8F9AE'
+
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 import random
 import json
 
-# Set up credentials and build API client
+
 youtube = build('youtube', 'v3', developerKey=api_key)
 
-# Set search parameters
+
 search_params = {
-    'q': 'music',
-    'type': 'video',
-    'part': 'id,snippet',
+    'chart': 'mostPopular',
+    'regionCode': 'US',
+    'part': 'snippet,statistics',
     'maxResults': 50
 }
 
-# Search for videos using the API
+
 try:
-    search_response = youtube.search().list(**search_params).execute()
+    search_response = youtube.videos().list(**search_params).execute()
     video_objects = []
     for item in search_response['items']:
-        video_id = item['id']['videoId']
+        video_id = item['id']
         video_url = f'https://www.youtube.com/watch?v={video_id}'
         video_name = item['snippet']['title']
         video_channel = item['snippet']['channelTitle']
-        video_thumbnail = item['snippet']['thumbnails']['default']['url']
+        video_thumbnail = item['snippet']['thumbnails']['high']['url']
         video_date = item['snippet']['publishedAt']
+        video_views = item['statistics']['viewCount']
         video_object = {
             'url': video_url,
             'name': video_name,
             'channel': video_channel,
             'thumbnail': video_thumbnail,
-            'date_uploaded': video_date
+            'date_uploaded': video_date,
+            'views': video_views
         }
         video_objects.append(video_object)
 
-    # Print the list of video objects as JSON
     print(json.dumps(video_objects, indent=4))
 
 except HttpError as e:
     print(f'An error occurred: {e}')
+
 
--- src/components/YTcard.vue
+++ src/components/YTcard.vue
@@ -1,18 +1,66 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-    <div class="yt-card-cont">
-
-    </div>
+	<div class="yt-card-cont">
+		<div class="yt-image">
+			<img src="https://i.ytimg.com/vi/qCm0uKgQxvc/hqdefault.jpg" alt="" />
+		</div>
+		<div class="name-avatar">
+			<div class="img-av">
+				<img src="https://yt3.ggpht.com/M4pctOKG0tLW2BKn_YsEdnkAI-dDiQt57cEsRpkO5DbDHPTPsfnwAynReLHCG4hsQ7-EOozqtQ=s68-c-k-c0x00ffffff-no-rj" alt="">
+			</div>
+			<div class="nm">
+				<p>Liverpool vs. Real Madrid: Extended Highlights | UCL Round of 16 - Leg 1 | CBS Sports Golazo</p>
+			</div>
+		</div>
+	</div>
 </template>
 
 <style scoped>
-.yt-card-cont{
-    width: 280px;
-    height: 250px;
-    background-color: blue;
-    margin: 0 8px 40px 8px;
+.name-avatar {
+    margin-top: 10px;
+    display: flex;
+    max-height: ;
+}
+.img-av {
+	width: 36px;
+	height: 36px;
+}
+.img-av img {
+    
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+    border-radius: 100%;
+}
+.nm {
+    margin: 0 10px;
+	padding:  0 5px;
+	width: 230px;
+	font-size: 12px;
+	font-weight: 900;
+	overflow: auto;
+}
+
+.yt-card-cont {
+	display: flex;
+	flex-direction: column;
+	width: 280px;
+	height: 250px;
+	border: 1px solid black;
+	margin: 0 8px 40px 8px;
+}
+
+.yt-image {
+	width: 100%;
+	height: 150px;
+	border-radius: 10px;
+}
+
+.yt-image img {
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+	border-radius: inherit;
 }
 </style>
